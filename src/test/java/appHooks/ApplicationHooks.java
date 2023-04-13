b1f43b1245e747e20914fdd4986e73e8f43f5828
Sneha <snehasai525@gmail.com>
2023-04-13 13:41:48 -0400

    app Hooks commit

---
+++ src/test/java/appHooks/ApplicationHooks.java
@@ -0,0 +1,43 @@
+package appHooks;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import com.qa.factory.Launch_Browser;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+public class ApplicationHooks {
+	
+WebDriver driver =Launch_Browser.launch_browser();
+	
+	/*
+	 * @After public void tearDown(Scenario scenario) { if(scenario.isFailed()) {
+	 * //take screenschot String screenshotName =
+	 * scenario.getName().replaceAll("","_"); byte[] sourcepath =
+	 * ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+	 * scenario.attach(sourcepath,"image/png", screenshotName);
+	 * 
+	 * }
+	 */
+    	
+    		
+    		Logger logger = LogManager.getLogger();
+
+    		@After
+    		public void takeScreenshotOnFailure(Scenario scenario) {
+
+    			logger.info("<takeScreenshotOnFailure>: inside");
+    			if (scenario.isFailed()) {
+
+    				logger.info("<takeScreenshotOnFailure>: inside scenario.isFailed()");
+    				TakesScreenshot ts = (TakesScreenshot) driver;
+
+    				byte[] src = ts.getScreenshotAs(OutputType.BYTES);
+    				scenario.attach(src, "image/png", "screenshot");
+    			}
+
+    		}
+
+
+}
